b89683c299c47690b6552ca43797494d41d77f77
Michal Adda <addamichal@gmail.com>
2023-04-30 22:25:01 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,15 @@
 // set op codes counter + rename it correctly based on ben eater's naming convention
 // add ui tests
 // use webpack?
+// add prettier
+
+// import { getInitState, nextInstruction, run } from "./emulator";
+// import { AddInstruction, HltInstruction, JcInstruction, JmpInstruction, LdaInstruction, LdiInstruction, OutInstruction, StaInstruction } from "./instructions";
+// import { State } from "./state";
+
+console.log('here??')
+
+setBinaryValue('a-register', 12);
 
 function setBinaryValue(module: string, number: number) {
     let leds = Array.from(document.querySelectorAll(`#${module} .led`)).reverse();
@@ -28,11 +37,68 @@ function setBinaryValue(module: string, number: number) {
     }
 }
 
-setBinaryValue('a-register', 42);
-setBinaryValue('b-register', 15);
-setBinaryValue('b-register', 42 + 15);
-setBinaryValue('counter', 15);
-setBinaryValue('bus', 17);
-setBinaryValue('memory-address', 8);
-setBinaryValue('memory-content', 30);
-setBinaryValue('instruction-register', 43);
+/*
+function updateUI(state: State) {
+    setBinaryValue('a-register', state.aRegister);
+    setBinaryValue('b-register', state.bRegister);
+    setBinaryValue('sum-register', state.sumRegister);
+    setBinaryValue('counter', state.counter);
+    setBinaryValue('bus', state.bus);
+    setBinaryValue('memory-address', state.memoryAddress);
+    setBinaryValue('memory-content', state.memoryContent);
+    setBinaryValue('instruction-register', state.instructionRegister);
+}
+*/
+
+function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/*
+async function start() {
+    console.log('start called!');
+    let state = getInitState();
+    state.ram[0] = new LdiInstruction(1).toNumber();
+    state.ram[1] = new StaInstruction(14).toNumber();
+    state.ram[2] = new LdiInstruction(0).toNumber();
+    state.ram[3] = new StaInstruction(15).toNumber();
+    state.ram[4] = new OutInstruction().toNumber();
+    state.ram[5] = new LdaInstruction(14).toNumber();
+    state.ram[6] = new AddInstruction(15).toNumber();
+    state.ram[7] = new StaInstruction(14).toNumber();
+    state.ram[8] = new OutInstruction().toNumber();
+    state.ram[9] = new LdaInstruction(15).toNumber();
+    state.ram[10] = new AddInstruction(14).toNumber();
+    state.ram[11] = new JcInstruction(13).toNumber();
+    state.ram[12] = new JmpInstruction(3).toNumber();
+    state.ram[13] = new HltInstruction().toNumber();
+
+    updateUI(state);
+    await sleep(1000);
+
+    state = nextInstruction(state);
+    updateUI(state);
+    await sleep(1000);
+
+    state = nextInstruction(state);
+    updateUI(state);
+    await sleep(1000);
+
+    state = nextInstruction(state);
+    updateUI(state);
+    await sleep(1000);
+
+    state = nextInstruction(state);
+    updateUI(state);
+    await sleep(1000);
+
+    state = nextInstruction(state);
+    updateUI(state);
+    await sleep(1000);
+
+}
+
+start();
+*/
+
+console.log('here?')
