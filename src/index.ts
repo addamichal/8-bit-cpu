918117406ad99aef18e4494fcf70456baf2b5de2
Michal Adda <addamichal@gmail.com>
2023-05-06 11:28:11 +0200

    fixing 7 segment display

--- src/index.ts
+++ src/index.ts
@@ -45,8 +45,8 @@ function setOutputValue(number: number) {
     if (digits.length !== 4) throw new Error('Seven Segment Display does not have enough digits');
 
     digits[0].textContent = number >= 0 ? '' : '-';
-    digits[1].textContent = hundredsPlace ? hundredsPlace.toString() : '';
-    digits[2].textContent = tensPlace ? tensPlace.toString() : '';
+    digits[1].textContent = hundredsPlace ? hundredsPlace.toString() : '0';
+    digits[2].textContent = tensPlace ? tensPlace.toString() : '0';
     digits[3].textContent = onesPlace ? onesPlace.toString() : '0';
 }
 
@@ -117,7 +117,7 @@ function init(): State {
     // state.ram[1] = new JzInstruction(15).toNumber();
     // state.ram[15] = 15;
 
-    state.ram[0] = new HltInstruction().toNumber();
+    // state.ram[0] = new HltInstruction().toNumber();
 
     // state.ram[0] = new LdiInstruction(15).toNumber();
     // state.ram[1] = new OutInstruction().toNumber();
@@ -128,20 +128,20 @@ function init(): State {
 
     // state.ram[0] = new HltInstruction().toNumber();
 
-    // state.ram[0] = new LdiInstruction(1).toNumber();
-    // state.ram[1] = new StaInstruction(14).toNumber();
-    // state.ram[2] = new LdiInstruction(0).toNumber();
-    // state.ram[3] = new StaInstruction(15).toNumber();
-    // state.ram[4] = new OutInstruction().toNumber();
-    // state.ram[5] = new LdaInstruction(14).toNumber();
-    // state.ram[6] = new AddInstruction(15).toNumber();
-    // state.ram[7] = new StaInstruction(14).toNumber();
-    // state.ram[8] = new OutInstruction().toNumber();
-    // state.ram[9] = new LdaInstruction(15).toNumber();
-    // state.ram[10] = new AddInstruction(14).toNumber();
-    // state.ram[11] = new JcInstruction(13).toNumber();
-    // state.ram[12] = new JmpInstruction(3).toNumber();
-    // state.ram[13] = new HltInstruction().toNumber();
+    state.ram[0] = new LdiInstruction(1).toNumber();
+    state.ram[1] = new StaInstruction(14).toNumber();
+    state.ram[2] = new LdiInstruction(0).toNumber();
+    state.ram[3] = new StaInstruction(15).toNumber();
+    state.ram[4] = new OutInstruction().toNumber();
+    state.ram[5] = new LdaInstruction(14).toNumber();
+    state.ram[6] = new AddInstruction(15).toNumber();
+    state.ram[7] = new StaInstruction(14).toNumber();
+    state.ram[8] = new OutInstruction().toNumber();
+    state.ram[9] = new LdaInstruction(15).toNumber();
+    state.ram[10] = new AddInstruction(14).toNumber();
+    state.ram[11] = new JcInstruction(13).toNumber();
+    state.ram[12] = new JmpInstruction(3).toNumber();
+    state.ram[13] = new HltInstruction().toNumber();
 
     updateUI(state);
 
